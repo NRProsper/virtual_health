8b54113a7a4a0e6c0bf37235c8c309f5723edf71
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-06-25 14:26:19 +0200

    Upload my local work

---
+++ src/App.vue
@@ -0,0 +1,88 @@
+<script setup>
+import Menu from "vue-material-design-icons/Menu.vue";
+import Close from "vue-material-design-icons/Close.vue";
+
+import {onMounted, ref, watch} from "vue";
+import HomeView from "@/views/HomeView.vue";
+
+const isOpen = ref(false);
+const toggleMenu = () => {
+  isOpen.value = !isOpen.value;
+}
+
+watch(isOpen, (newVal) => {
+  if(newVal) {
+    document.body.classList.add("no-scroll");
+  }else {
+    document.body.classList.remove("no-scroll");
+  }
+})
+
+
+</script>
+
+<template>
+  <header class="flex">
+    <div class="container">
+      <div class="navigation flex justify-between">
+        <a href="#" class="logo">
+          <img src="./assets/img/logo.svg" alt="Logo Image">
+        </a>
+        <nav :class="['nav-links', {open : isOpen}]">
+          <a href="#" class="active">Home</a>
+          <a href="#">Find doctor</a>
+          <a href="#">Apps</a>
+          <a href="#">Testimonials</a>
+          <a href="#">Contact us</a>
+        </nav>
+        <div class="menu-toggle" @click="toggleMenu">
+          <Menu v-if="!isOpen" />
+          <Close v-else />
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <main>
+    <HomeView />
+  </main>
+</template>
+
+<style scoped>
+@media only screen and (max-width: 768px) {
+  .nav-links {
+    //display: none;
+    position: absolute;
+    top: 80px;
+    left: 0;
+    right: 0;
+    height: calc(100vh -  80px);
+    visibility: hidden;
+    background: var(--vh-body-background);
+    width: 100vw;
+    //transition: all 0.3s ease-in-out;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-flow: column;
+    gap: 20px;
+  }
+
+  .nav-links a {
+    margin-right: 0;
+    border-bottom: 1px solid var(--vh-nav-text);
+    width: 200px;
+    text-align: center;
+  }
+
+  .nav-links.open {
+    visibility: visible;
+  }
+
+
+  .menu-toggle {
+    display: inline-flex;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,51 @@
+<script setup>
+
+</script>
+
+<template>
+  <div id="main-content">
+    <div class="container">
+      <div class="hero flex">
+        <div class="left">
+          <h1>Virtual healthcare <br /> for you</h1>
+          <p>Trafalgar provides progressive, and affordable
+            <br/>healthcare, accessible on mobile and online
+            for everyone</p>
+          <div class="cta">
+            <a href="#" class="btn btn-primary">Consult today</a>
+          </div>
+        </div>
+        <div class="right">
+          <img src="../assets/img/main-illustration.png" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.hero {
+  gap: 50px;
+  margin-top: 40px;
+}
+
+.left {
+  flex:1;
+}
+
+.right {
+  flex: 1;
+}
+
+.left h1{
+  color: var(--vh-headers);
+  font-size: 48px;
+  line-height: 56px;
+  margin: 12px 0;
+}
+
+.left p {
+  margin-bottom: 30px;
+}
+
+</style>
